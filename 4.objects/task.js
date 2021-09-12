463f948af19f137ed7d4c81bdda8f4660a38191d
galilinka <gali_82@mail.ru>
2021-09-12 23:39:54 +0300

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
-function Student(name, gender, age) {
-    // Ваш код
+"use strict";
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
+
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  console.log(this.subject = subjectName);
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+    } else {
+      for (let i = 0; i < marks.length; i++){
+        this.marks.push(marks[i]);
+      }
+    }
+}
+
+Student.prototype.getAverage = function (arr) {
+  for (let i = 0, sum = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum/i;
+
+  this.average= arr;
+}
+
+Student.prototype.exclude = function (reason) {
+  console.log(this.exclude = reason);
+  delete Student.subject; 
+  delete Student.marks; 
+}
