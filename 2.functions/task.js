7a53540e452b4c37396eb7e973dfdb8a1a89fc77
galilinka <gali_82@mail.ru>
2021-09-15 23:33:55 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,79 @@
+"use strict";
+
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+let min = 0;
+let max = 0;
+let avg = 0;
+let total = 0;
 
   // Ваш код
 
-  return { min:min, max:max, avg:avg };
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] > max){
+      max = arr[i];
+    }
+
+    if (arr[i] < min){
+      min = arr[i];
+    }
+
+    total = total + arr[i];
+  }
+
+  avg = +(total/arr.length).toFixed(2);
+
+  return { min, max, avg};
 }
+
+//console.log(getArrayParams([-99, 99, 10]));
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
 
-  return sum;
+  return sum
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
+  let temp;
 
-  // Ваш кода
-  // for ...
+  for (let i = 0; i < arrOfArr.length; i++){
+    temp = func(arrOfArr[i]);
+    if(temp > max){
+      max = temp;
+    }
+  }
   
   return max
 }
 
+let a = [[1,5,100], [4,5,6]];
+console.log(makeWork(a, worker2));
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = 0;
+  let max = 0;
+  let temp = [];
+
+   for (let i = 0; i < arr.length; i++){
+     temp.push(Math.abs(arr[i]));
+   }
+
+   temp = temp.sort((a,b) => a - b);
+
+   min = temp[0];
+   max = temp[temp.length - 1];
+
+   return max - min
+
 }
+
+
