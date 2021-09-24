2c0b8cab713cdfefea8f7456c3d3da40e66b0df1
galilinka <gali_82@mail.ru>
2021-09-24 19:20:10 +0300

    another commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -25,11 +25,8 @@ Student.prototype.addMark = function (mark) {
 Student.prototype.addMarks = function (...marks) {
   if(this.marks === undefined){ 
     this.marks = [];
-    } else {
-      for (let i = 0; i < marks.length; i++){
-        this.marks.push(marks[i]);
-      }
-    }
+    } 
+      this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
